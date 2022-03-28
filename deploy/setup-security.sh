7ac1509252790e22054ca55e00cc44a1e3be5e1e
Jeff Glatz <jeff@bestquality.engineering>
2022-03-27 20:03:56 -0500

    More workflow setup

--- deploy/setup-security.sh
+++ deploy/setup-security.sh
@@ -1,16 +1,8 @@
 #!/bin/bash -e
 
-write_settings_security() {
-  cat <<EOF >~/.m2/settings-security.xml
-<?xml version="1.0" encoding="UTF-8"?>
-<settingsSecurity>
-    <master>${MAVEN_SECURITY_MASTER}</master>
-</settingsSecurity>
-EOF
-}
-
 # Setup maven settings-security.xml for nexus staging
-write_settings_security
+echo "Installing ~/.m2/settings-security.xml"
+printenv SETTINGS_SECURITY_XML > ~/.m2/settings-security.xml
 
 # Setup the code signing key; it will be used to publish snapshots and releases
 printenv CODE_SIGNING_KEY > /tmp/code-signing-key.asc
