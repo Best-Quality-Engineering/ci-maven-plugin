95bce03706d3422d8ce60ef151eaaaf210243330
Jeff Glatz <jeff@bestquality.engineering>
2022-04-29 19:58:41 -0500

    Test site publishing

--- deploy/build-branch.sh
+++ deploy/build-branch.sh
@@ -5,4 +5,6 @@ if [ "${GITHUB_REF_TYPE}" != "tag" ]; then
   mvn -e -B -ntp -P ci clean install -Dsha1="-${GITHUB_RUN_ID}"
   echo "Uploading code coverage report for ${GITHUB_REF_NAME}"
   bash <(curl -s https://codecov.io/bash)
+  echo "Publishing site to GitHub Pages"
+  mvn -e -B -ntp -P ci site -Dsha1="-${GITHUB_RUN_ID}"
 fi
